e628e7d2bd5648d0b1384d1475b83c8b41786534
salimamir99 <137554042+salimamir99@users.noreply.github.com>
2023-07-10 18:33:39 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+//alert("Login JS");
+
+const correctEmail = 'jwd@gmail.com';
+const correctPassword = "1234";
+
+const messageContainer = document.getElementById('message-container');
+
+function cekLogin(){
+    const userEmail = document.getElementById('fEmail').value;
+    const userPassword = document.getElementById('fPassword').value;
+
+    if (userEmail != correctEmail || userPassword != correctPassword){
+        messageContainer.innerHTML = "<p class='error-message'>Login Gagal, Email atau password salah !</p>;" 
+    }else{
+        messageContainer.innerHTML = "<p class='success-message'>Login Berhasil :) </p>;" 
+    }
+}
+
